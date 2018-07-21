918760d067f9f082e3ab021d7921551a4899d4a1
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 05:16:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  new_arr = []
+  
+  if collection.length == 0 
+   return nil 
+  else 
+  idx = 0 
+    while idx < collection.length 
+     new_arr << collection[idx]  if yield(collection[idx]) == true
+      idx += 1
+     end 
+  end
+  new_arr
 end
