58c787a72e907c3bf33747a08194962fa6030088
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 05:13:44 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ if collection.length == 0 
+   return nil 
+else 
+  idx = 0 
+    while idx < collection.legnth 
+    yield(collection[idx])
+    idx += 1
+  end 
 end
