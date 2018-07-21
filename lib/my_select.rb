58e5538afec7ccad1f99403742255d82bd7defb7
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 05:14:50 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,12 @@
 def my_select(collection)
+  new_arr = []
+  
   if collection.length == 0 
    return nil 
   else 
   idx = 0 
     while idx < collection.legnth 
-      yield(collection[idx])
+     new_arr << if yield(collection[idx])
       idx += 1
      end 
   end
